269180a67003ea993dc26ae56709b13e8111381f
Sony <malakmuhammadhassan>
2022-04-25 02:49:13 -0700

    ASSIGNMENT_02

---
+++ app/src/main/java/com/example/fa19_bse_003_assignment_02/safee.java
@@ -0,0 +1,23 @@
+package com.example.fa19_bse_003_assignment_02;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class safee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_safee);
+    }
+}
